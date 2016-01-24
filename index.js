915aa04f7998615c10fa1bbd4cb16155d637c46b
Kyle Mitchell <kyle@kemitchell.com>
2016-01-24 10:31:21 -0800

    Remove semicolons

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-var satisfies = require('spdx-satisfies');
+var satisfies = require('spdx-satisfies')
 
-var uses = ['use', 'link', 'modify'];
+var uses = ['use', 'link', 'modify']
 
-var endOfLine = /\r?\n/;
-var comment = /^#/;
-var empty = /^\s*$/;
-var rule = new RegExp('^(' + uses.join('|') + '): (.+)$');
+var endOfLine = /\r?\n/
+var comment = /^#/
+var empty = /^\s*$/
+var rule = new RegExp('^(' + uses.join('|') + '): (.+)$')
 
 var allowAnyLicense = function() {
-  return true;
-};
+  return true
+}
 
 module.exports = function(fileContent) {
-  var returned = {};
+  var returned = {}
 
   uses
     .forEach(function(use) {
-      returned[use] = allowAnyLicense;
-    });
+      returned[use] = allowAnyLicense
+    })
 
   fileContent
     .split(endOfLine)
     .forEach(function(line) {
       if (!empty.test(line) && !comment.test(line)) {
-        var match = rule.exec(line);
+        var match = rule.exec(line)
         if (match) {
-          var use = match[1];
-          var expression = match[2];
+          var use = match[1]
+          var expression = match[2]
           if (uses.indexOf(use) < 0) {
-            throw new Error('Invalid use, "' + use + '"');
+            throw new Error('Invalid use, "' + use + '"')
           }
           returned[use] = function(argument) {
-            return satisfies(argument, expression);
-          };
+            return satisfies(argument, expression)
+          }
         } else {
-          throw new Error('Invalid line: "' + line + '"');
+          throw new Error('Invalid line: "' + line + '"')
         }
       }
-    });
+    })
 
-  return returned;
-};
+  return returned
+}
 
-module.exports.implementationVersion = '0.1.0';
-module.exports.specificationVersion = '0.0.0-prerelease';
+module.exports.implementationVersion = '0.1.0'
+module.exports.specificationVersion = '0.0.0-prerelease'
